86df9300e551fff5950a4e6dae29f7b195a35fb9
Or4nG.M4n <a.ornga@gmail.com>
2022-09-27 10:41:31 +0300

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,14 +6,14 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>! Or4ng.m4n !</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Or4nG.M4n" />
         <p className="description">
-          Get started by editing <code>pages/index.js</code>
+          Twitter <code>@interestedz</code>
         </p>
       </main>
 
